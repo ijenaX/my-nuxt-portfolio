ad69e0512b3f2be1a0cc85987361d159d92a944d
SvenjaG <svenja_galla@web.de>
2025-07-17 20:02:32 +0200

    design wip and responsive home page

--- app/pages/home.vue
+++ app/pages/home.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts"></script>
 
 <template>
-  <section class="container mx-auto max-w-7xl flex justify-center items-center gap-8 px-4 pt-14">
+  <section class="container mx-auto max-w-7xl flex flex-col lg:flex-row justify-center items-center gap-8 px-4 pt-14">
     <!-- Textblock -->
-    <div class="flex-1 max-w-2xl">
-      <h1 class="leading-tight">Willkommen in meinem Portfolio</h1>
+    <div class="max-w-2xl">
+      <h1 class="leading-tight">Technik trifft Design</h1>
       <p class="text-xl leading-relaxed mb-8">
         Ich bin Svenja, Softwareentwicklerin aus NRW. <br />
-        Digitale Gestalterin mit klarem Blick für das Zusammenspiel von Form und Funktion. Ich gestalte, entwickle und verbinde Welten. Neugier treibt mich, Vielfalt inspiriert mich.
+        Generalistin und digitale Gestalterin. Entwickeln ist meine Leidenschaft. Von der visuellen Komposition bis zur technischen Umsetzung. Ich verbinde Welten, stets von Neugier getrieben und von Vielfalt inspiriert.
       </p>
       <div class="flex gap-4">
         <NuxtLink to="/techstack" class="flex items-center gap-2 px-6 py-3 font-bold text-accent hover:bg-accent hover:text-white transition"><Icon name="mdi:arrow-top-right" />Tech Stack</NuxtLink>
@@ -16,8 +16,8 @@
     </div>
 
     <!-- Bildblock -->
-    <div class="flex flex-1 justify-center py-6">
-      <NuxtImg src="/images/galla-bust-website.png" width="290" format="webp" alt="Roman Bust with Svenjas face" class="transform -scale-x-100" />
+    <div class="justify-center py-6">
+      <NuxtImg src="/images/galla-bust-website-left.png" width="290" format="webp" alt="Roman Bust with Svenjas face" />
     </div>
   </section>
 </template>
